a38ade960805b70eb83d5c8d0ebf9978fb709c53
GLASMAN Conor <c.glasman@bravosolution.com>
2018-08-08 12:26:27 +0100

    Adding the corpus building for the review list

--- NLP.py
+++ NLP.py
@@ -7,13 +7,19 @@ import pandas as pd
 import re
 import nltk
 from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+
 
 #Importing the dataset
 dataset = pd.read_csv('Restaurant_Reviews.tsv', delimiter='\t', quoting = 3)
 
 #Cleaning the texts
-review1 = dataset['Review'][0]
-review = re.sub(r'[^\w]', r' ' ,review1)
-review = review.lower()
-review = review.split()
-review = [w for w in review if not w in stopwords.words('english')]
+corpus = []
+for i in range(0,len(dataset)):
+    review = re.sub(r'[^\w]', r' ' ,dataset['Review'][i])
+    review = review.lower()
+    review = review.split()
+    ps = PorterStemmer()
+    review = [ps.stem(w) for w in review if not w in stopwords.words('english')]
+    review = ' '.join(review)
+    corpus.append(review)
