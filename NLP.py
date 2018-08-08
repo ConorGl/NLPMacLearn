505ade81865a9f02e95d114f8a2ad853cf3ce0d0
GLASMAN Conor <c.glasman@bravosolution.com>
2018-08-08 11:32:54 +0100

    Adding some cleaning to the data

--- NLP.py
+++ NLP.py
@@ -4,6 +4,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+import re
 
 #Importing the dataset
 dataset = pd.read_csv('Restaurant_Reviews.tsv', delimiter='\t', quoting = 3)
+
+#Cleaning the texts
+review1 = dataset['Review'][0]
+review = re.sub(r'[^\w]', r' ' ,review1)
+review = review.lower()
