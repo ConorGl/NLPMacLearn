c44d07dbeac3a934a90d9fe989ee5ff220e1c8a6
GLASMAN Conor <c.glasman@bravosolution.com>
2018-08-08 11:12:37 +0100

    Starting to read from the tsv file

--- NLP.py
+++ NLP.py
@@ -1,14 +1,9 @@
-from bs4 import BeautifulSoup
-import urllib.request 
-import re
-import nltk
+#Natural Language Processing
 
-#def get_key_words_from (STR)
-response = urllib.request.urlopen('https://factba.se/transcript/donald-trump-speech-maga-lewis-center-oh-august-4-2018') 
-html = response.read() 
-soup = BeautifulSoup(html,"html.parser") 
-text = soup.get_text(strip=True) 
-tokens = [t for t in re.split(r"[\s\d]",text)]
-freq = nltk.FreqDist(tokens)
-for key,val in freq.items():
-    print (str(key) + ' - ' + str(val) )
+#Import the key libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#Importing the dataset
+dataset = pd.read_csv('Restaurant_Reviews.tsv', delimiter='\t', quoting = 3)
