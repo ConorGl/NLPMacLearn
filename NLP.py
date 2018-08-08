ead604a3eb12be8b9450b2dc198920af734aba73
GLASMAN Conor <c.glasman@bravosolution.com>
2018-08-08 11:53:17 +0100

    Adding some tokenization

--- NLP.py
+++ NLP.py
@@ -5,6 +5,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import re
+import nltk
+
+nltk.download('stopwords')
 
 #Importing the dataset
 dataset = pd.read_csv('Restaurant_Reviews.tsv', delimiter='\t', quoting = 3)
@@ -13,3 +16,4 @@ dataset = pd.read_csv('Restaurant_Reviews.tsv', delimiter='\t', quoting = 3)
 review1 = dataset['Review'][0]
 review = re.sub(r'[^\w]', r' ' ,review1)
 review = review.lower()
+words = [w for w in re.split(r'[^\w]', review)]
